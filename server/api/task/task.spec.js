ce4669b76140a302937ce53af10ecad47d9d9c45
seraya <takahser@students.zhaw.ch>
2015-04-20 00:33:56 +0200

    fixed bugs

--- server/api/task/task.spec.js
+++ server/api/task/task.spec.js
@@ -17,4 +17,27 @@ describe('GET /api/tasks', function() {
         done();
       });
   });
+
+/*
+  it('should return an array of task objects', function() {
+    request(app)
+      .get('/api/tasks')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        //res.body.should.be.instanceof(Array);
+        console.log("checking jsm specs");
+        console.log(res.body);
+        expect(res.body[0]._id).toBeDefined();
+        expect(res.body[0].name).toBeDefined();
+        expect(res.body[0].info).toBeDefined();
+        expect(res.body[0].estimation).toBeDefined();
+        expect(res.body[0].status).toBeDefined();
+        expect(res.body[0].personResponsible).toBeDefined();
+        done();
+      });
+  });
+*/
+
 });
